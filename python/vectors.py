399bd24248e9c75bbd733838b8bc67bb6911f525
Aviv Kaveh <aviv.kaveh@gmail.com>
2026-07-15 06:39:27 +0300

    Implement vector addition

--- python/vectors.py
+++ python/vectors.py
@@ -22,4 +22,7 @@ def add(v1, v2):
     list[float]
         The vector sum.
     """
-    pass
+    if len(v1) != len(v2):
+        raise ValueError("Vectors must have the same dimension.")
+
+    return [a + b for a, b in zip(v1, v2)]
