dc855ec176ff7258b21cece0b5fe4c4dc1e11348
Aviv Kaveh <aviv.kaveh@gmail.com>
2026-07-15 06:36:32 +0300

    Add vector module

--- python/vectors.py
+++ python/vectors.py
@@ -4,3 +4,22 @@ Vector operations.
 This module contains implementations of basic vector operations
 used throughout the linear algebra package.
 """
+
+
+def add(v1, v2):
+    """
+    Return the sum of two vectors.
+
+    Parameters
+    ----------
+    v1 : list[float]
+        First vector.
+    v2 : list[float]
+        Second vector.
+
+    Returns
+    -------
+    list[float]
+        The vector sum.
+    """
+    pass
